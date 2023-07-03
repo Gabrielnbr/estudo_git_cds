48dc606081a4aeee319911db49afa4cde0868496
Gabriel Nobre <gabrielnobregalvao@gmail.com>
2023-07-03 19:20:13 -0300

    Função main adicionada

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import streamlit as st
 
 def load_data():
     return pd.read_csv('../Projeto/data/processed/bikes_completed.csv')
+
+def main():
+    df = load_data()
+
+    st.dataframe(df)
+    
+if __name__ == '__main__'
+    main()
